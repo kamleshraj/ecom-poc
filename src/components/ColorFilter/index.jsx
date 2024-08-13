605c6d46b775165a308a93e22c52d12ff728495c
kamlesh kumar <kamlesh.kumar@ics-global.in>
2024-08-13 12:38:41 +0530

    fixed the color filter issue

--- src/components/ColorFilter/index.jsx
+++ src/components/ColorFilter/index.jsx
@@ -5,7 +5,7 @@ import styles from './ColorSidebar.module.scss'
 
 export const ColorFilter = () => {
   const dispatch = useDispatch();
-  const {selectedColors,products} = useSelector((state) => state.products);
+  const {selectedColors,products,filteredProducts} = useSelector((state) => state.products);
   const colors = ["Red", "Blue", "Green", "Yellow", "Black", "White"];
   
   const handleCheckboxChange = (color) => {
@@ -18,8 +18,9 @@ export const ColorFilter = () => {
     dispatch(setSelectedColors(updatedColors));
   };
 
-  const getColorCount = (color) => {
-    return products.filter((product) => product.color === color).length;
+  const getColorCount = (color, filteredProducts = null, products) => {
+    const productList = filteredProducts.length > 0 ? filteredProducts : products;
+    return productList.filter((product) => product.color === color).length;
   };
 
   return (
@@ -34,8 +35,8 @@ export const ColorFilter = () => {
             checked={selectedColors.includes(color)}
           />
           <div className={styles.colorsBox} style={{ backgroundColor: `${color}` }} />
-          <div>
-             {color} ({getColorCount(color)})
+          <div className="color-item">
+             {color} ({getColorCount(color,filteredProducts,products)})
           </div>
         </div>
       ))}
