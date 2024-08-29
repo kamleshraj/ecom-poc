4c73d83638c7d15c458983390c995433399a354f
kamlesh kumar <kamlesh.kumar@ics-global.in>
2024-08-29 09:50:02 +0530

    changes in the scss

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,40 +2,12 @@ import { useSelector } from "react-redux";
 import FilterSelect from "../components/FilterSelect";
 import ShopList from "../components/ShopList";
 import useWindowScrollToTop from "../hooks/useWindowScrollToTop";
-import styled from "styled-components";
 import {CustomBreadcrumbs} from "../components/CustomBreadCrumbs";
 import {PriceRangeSlider} from "../components/PriceRangeSlider";
 import { Helmet } from "../components";
 import {ColorFilter} from "../components/ColorFilter";
 
-const MainWrapper = styled.div`
-  position: relative;
-  .innerWrapper {
-    display: flex;
-    justify-content: center;
-    .category-sidebar {
-      position: -webkit-sticky;
-      position: sticky;
-      top: 0;
-      align-self: flex-start;
-      padding: 1rem;
-      flex: 2;
-      display: flex;
-      flex-direction: column;
-    }
-    .productsWrapper {
-      flex: 8;
-      display: grid;
-      gap: 15px;
-      padding: 30px 30px 45px;
-      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-      background-color: #f6f9fc;
-    }
-    .card-width {
-      grid-template-columns: repeat(auto-fit, minmax(200px, 240px));
-    }
-  }
-`;
+import styles from '../pages/commonStyle.module.scss'
 
 const Shop = () => {
   const {filteredProducts,products} = useSelector((state) => state.products);
@@ -45,24 +17,24 @@ const Shop = () => {
   return (
     <>
       <Helmet title="Shop">
-        <MainWrapper>
+        <section className={styles.shopWrapper}>
           <CustomBreadcrumbs />
-          <div className="innerWrapper">
-            <aside className="category-sidebar">
-              <h5 className="section-title">Filters by</h5>
+          <div className={styles.innerWrapper}>
+            <aside className={styles.categorySidebar}>
+              <h5 className={styles.sectionTitle}>Filters by</h5>
               <FilterSelect />
               <PriceRangeSlider />
               <ColorFilter />
             </aside>
             <div
-              className={`productsWrapper ${
-                filteredProducts.length > 3 ? "null" : "card-width"
+              className={`${styles.productsWrapper} ${
+                filteredProducts.length > 3 ? "null" : "cardWidth"
               }`}
             >
               <ShopList productItems={isFiltered ? filteredProducts:products} />
             </div>
           </div>
-        </MainWrapper>
+        </section>
       </Helmet>
     </>
   );
